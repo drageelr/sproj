666d0c852044672b386af056c2100433afdd9cde
Hammad Nasir <hammadn99@gmail.com>
2022-04-16 02:23:00 +0500

    pm2 config file added

---
+++ ecosystem.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    apps : [{
+      name   : "dispatcher",
+      script : "./dispatcher.js"
+    },{
+      name   : "server",
+      script : "./server/bin/www.js"
+    },{
+        name   : "tool-controller",
+        script : "./tool-controller/tool-controller.js"
+    }]
+  }
+  
